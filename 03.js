bb89dd535c2690516bd08c9886c717c392f9f7f2
leo demaria <leodemaria07@gmail.com>
2023-09-13 02:04:21 -0300

    will realizado

--- 03.js
+++ 03.js
@@ -4,6 +4,7 @@
   código dentro de las funciones ya definidas. 
   No comentar la funcion 
 */
+
 function buscarAmigo(amigos, nombre) {
   // La funcion llamada 'buscarAmigo' recibe como argumento un array llamado 'amigos' que contiene
   // en cada posición del arreglo un objeto que tiene como propiedades 'nombre' y 'edad'. También
@@ -13,9 +14,16 @@ function buscarAmigo(amigos, nombre) {
   //  var amigos = [{ nombre: 'toni', edad: 33 } , { nombre: 'Emi', edad: 25 }];
   //  buscarAmigo(amigos, 'toni') debe devolver { nombre: 'toni', edad: 33 };
 
-  // Tu código aca:
-  
+ 
+
+  for (let i = 0; i < amigos.length; i++) {
+    if(amigos[i].nombre === nombre){
+      return amigos[i];
+    }
+  }
 }
+ let amigos = [{nombre: 'toni', edad: 33}, {nombre: 'Emi', edad: 25}]; 
+ console.log(buscarAmigo(amigos, 'toni'));
 
 // No modifiques nada debajo de esta linea //
 
